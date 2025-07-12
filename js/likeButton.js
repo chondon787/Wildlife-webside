527859fa76d85dfbc7101e2fa7c6c95b5bb19860
chondon787 <chondonroy787@gmail.com>
2025-07-12 11:48:45 +0600

    header logo added

--- js/likeButton.js
+++ js/likeButton.js
@@ -1,10 +1,15 @@
 const likeButton = (like) =>{
+     
+ 
+
+    
 const likeContainer = document.getElementById('like-container');
 const div = document.createElement('div');
-// div.classList.add("grid","grid-cols-2","gap-5")
+ 
 div.innerHTML=`
-<div class="lg:w-28 w-60 lg:h-24 h-40 mx-auto  mb-3">
- <img class="w-full h-full rounded-sm" src=${like}/>
+<div class="lg:w-28 w-56 lg:h-24 h-40 mx-auto  mb-3">
+ <img class="w-full h-full rounded-sm object-cover" src="${like}"/>
+ </div>
 `
 likeContainer.appendChild(div)
 }
